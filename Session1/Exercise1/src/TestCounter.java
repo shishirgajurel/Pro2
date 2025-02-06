7a1e1baae59755248154df5bda2a04aa1794d2d8
Shishir <shishirgajurel999@gmail.com>
2025-02-06 09:51:06 +0100

    Pro 2 codes

--- Session1/Exercise1/src/TestCounter.java
+++ Session1/Exercise1/src/TestCounter.java
@@ -10,13 +10,12 @@ public class TestCounter
     Thread thread1 = new Thread(incrementer1, "Incrementer 2");
     Thread thread2 = new Thread(incrementer1, "Incrementer 2");
     thread.start();
-    thread.sleep(200);
+    Thread.sleep(200);
     thread1.start();
-    thread.join();
-    thread1.sleep(200);
+    Thread.sleep(200);
     thread2.start();
-    thread2.sleep(200);
-    thread.join();
+    Thread.sleep(200);
+
     System.out.println(Thread.currentThread().getName() + " : " + counter.getValue());
   }
 }
